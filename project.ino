cb55a7d1520f6b03035554c181450a70461244fc
Nattanan Semathong <32818460+Mingmon@users.noreply.github.com>
2018-02-09 02:19:15 +0700

    #add firebase switch light

---
+++ project.ino
@@ -0,0 +1,45 @@
+#include <Firebase.h>
+#include <FirebaseArduino.h>
+#include <FirebaseCloudMessaging.h>
+#include <FirebaseError.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseObject.h>
+
+#include <ESP8266WiFi.h>
+
+
+#define WIFI_SSID       "JIA-2409"
+#define WIFI_PASSWORD   "27791462"
+
+#define FIREBASE_HOST "testproject-abf8e.firebaseio.com"
+#define FIREBASE_KEY "NGgVOJ4iKLlp5fM00v6m2cCaaQ3DqEkbBsyBY0RK"
+#define D0 16
+#define LED D0
+void setup() {
+  Serial.begin(9600);
+  Serial.println(WiFi.localIP());
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  // Do something
+
+   Firebase.begin(FIREBASE_HOST, FIREBASE_KEY);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  
+
+   int name = Firebase.getInt("light/switch");
+Serial.print("Switch : ");
+ digitalWrite(13, name);
+  delay(500);
+Serial.println(name);
+}
